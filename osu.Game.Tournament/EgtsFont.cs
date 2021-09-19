6bd310c676bd67957e294131acda8517f26d19d9
Olle Kelderman <omkelderman@gmail.com>
2021-09-19 16:35:56 +0200

    add font

---
+++ osu.Game.Tournament/EgtsFont.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.Sprites;
+using osu.Game.Graphics;
+
+namespace osu.Game.Tournament
+{
+    public static class EgtsFont
+    {
+        public static FontUsage RedHatDisplay => GetFont();
+
+        public static FontUsage GetFont(float size = OsuFont.DEFAULT_FONT_SIZE, FontWeight weight = FontWeight.Regular, bool italics = false)
+            => new FontUsage("RedHatDisplay", size, weight.ToString(), italics);
+    }
+}
